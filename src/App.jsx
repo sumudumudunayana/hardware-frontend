b26763010ab22fdf253b08136aea50a0ec189413
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-09 18:01:31 +0530

    Add routing for company and distributor management pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ import ViewItemPage from "./pages/ViewItemPage";
 import UpdateItemPage from "./pages/UpdateItemPage";
 import SupplierOptionPage from "./pages/SupplierOptionPage";
 import AddCompanyPage from "./pages/AddCompanyPage";
+import CompanyOptionPage from "./pages/CompanyOptionPage";
+import ManageCompanyPage from "./pages/ManageCompanyPage";
+import CategoryOptionPage from "./pages/CategoryOptionPage";
+import AddCategoryPage from "./pages/AddCategoryPage";
+import ManageCategoryPage from "./pages/ManageCategoryPage";
+import DistributorOptionPage from "./pages/DistributorOptionPage";
+import AddDistributorPage from "./pages/AddDistributorPage";
+import ManageDistributorPage from "./pages/ManageDistributorPage";
 
 function App() {
   return (
@@ -19,7 +27,15 @@ function App() {
         <Route path="/ViewItemPage" element={<ViewItemPage />} />
         <Route path="/UpdateItemPage" element={<UpdateItemPage />} />
         <Route path="/SupplierOptionPage" element={<SupplierOptionPage />} />
+        <Route path="/CompanyOptionPage" element={<CompanyOptionPage />} />
         <Route path="/AddCompanyPage" element={<AddCompanyPage />} />
+        <Route path="/ManageCompanyPage" element={<ManageCompanyPage />} />
+        <Route path="/CategoryOptionPage" element={<CategoryOptionPage />} />
+        <Route path="/AddCategoryPage" element={<AddCategoryPage />} />
+        <Route path="/ManageCategoryPage" element={<ManageCategoryPage />} />
+        <Route path="/DistributorOptionPage" element={<DistributorOptionPage />} />
+        <Route path="/AddDistributorPage" element={<AddDistributorPage />} />
+        <Route path="/ManageDistributorPage" element={<ManageDistributorPage />} />
       </Routes>
     </HashRouter>
   );
