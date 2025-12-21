bc5ba693ef73579639f258827768b787b5d8f7b7
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-21 22:36:42 +0530

    cleaned the code

--- src/pages/ItemOptionPage.jsx
+++ src/pages/ItemOptionPage.jsx
@@ -13,31 +13,20 @@ export default function ItemOptionPage() {
         <h1 className="item-title">Item Management</h1>
 
         <div className="item-grid">
-
-          <div
-            className="item-box"
-            onClick={() => navigate("/AddItemPage")}
-          >
+          <div className="item-box" onClick={() => navigate("/AddItemPage")}>
             <h2>Add Item</h2>
             <p>Create and register new inventory items.</p>
           </div>
 
-          <div
-            className="item-box"
-            onClick={() => navigate("/ViewItemPage")}
-          >
+          <div className="item-box" onClick={() => navigate("/ViewItemPage")}>
             <h2>View Items</h2>
             <p>View all items with full details and stock info.</p>
           </div>
 
-          <div
-            className="item-box"
-            onClick={() => navigate("/UpdateItemPage")}
-          >
+          <div className="item-box" onClick={() => navigate("/UpdateItemPage")}>
             <h2>Update Item</h2>
             <p>Modify item data, prices, categories, etc.</p>
           </div>
-
         </div>
       </div>
     </div>
