0e1a0a36ddb3056028b962d6b15a335d5a35484c
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-21 22:37:16 +0530

    cleaned the code

--- src/pages/ManageCategoryPage.jsx
+++ src/pages/ManageCategoryPage.jsx
@@ -20,7 +20,6 @@ export default function ManageCategoryPage() {
 
   const [alert, setAlert] = useState({ show: false, type: "", message: "" });
 
-  // Load categories
   const loadCategories = async () => {
     try {
       const res = await axios.get("http://localhost:8080/category/get-all");
