168f4d4e485e982f8c4929f66f513640676092fc
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-21 22:36:47 +0530

    cleaned the code

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -12,9 +12,9 @@ export default function LandingPage() {
       <div className="content">
         <h1 className="title">Hardware Management System</h1>
         <p className="subtitle">
-          Manage inventory, bookings, customers, and hardware workflow all in one place.
+          Manage inventory, bookings, customers, and hardware workflow all in
+          one place.
         </p>
-
         <button className="start-btn" onClick={() => navigate("/OptionPage")}>
           Get Started
         </button>
