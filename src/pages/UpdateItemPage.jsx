9e7c8ca59e6177aa8156eff63e10b90d26c33096
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-25 23:14:36 +0530

    cleaned the code

--- src/pages/UpdateItemPage.jsx
+++ src/pages/UpdateItemPage.jsx
@@ -27,7 +27,6 @@ export default function UpdateItemPage() {
   const [deleteId, setDeleteId] = useState(null);
   const [alert, setAlert] = useState({ show: false, type: "", message: "" });
 
-  // Load dropdown data
   const loadDropdownData = async () => {
     try {
       const [catRes, comRes, distRes] = await Promise.all([
