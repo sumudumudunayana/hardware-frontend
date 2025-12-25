d36994140411a1e5c89ced46142be80a349babef
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2025-12-25 23:16:15 +0530

    cleaned the code

--- src/pages/UpdateItemPage.jsx
+++ src/pages/UpdateItemPage.jsx
@@ -237,7 +237,6 @@ export default function UpdateItemPage() {
               />
             </div>
 
-            {/* Category Dropdown */}
             <div className="form-group">
               <label>Category</label>
               <select
