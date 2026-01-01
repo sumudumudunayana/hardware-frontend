1c8dff82b9fc1947757d4ff819a1836cc3193cef
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-01 22:10:47 +0530

    cleaned the code

--- src/pages/ManageCategoryPage.jsx
+++ src/pages/ManageCategoryPage.jsx
@@ -61,7 +61,6 @@ export default function ManageCategoryPage() {
         editData
       );
       if (![200, 202, 204].includes(res.status)) throw new Error();
-
       const updated = categories.map((c) =>
         c.id === editData.id ? editData : c
       );
