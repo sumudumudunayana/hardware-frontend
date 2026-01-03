959e2fca3c22fd99c01c8d6e6475e059d3783010
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-03 23:27:16 +0530

    cleaned the code

--- src/pages/ManageCompanyPage.jsx
+++ src/pages/ManageCompanyPage.jsx
@@ -8,7 +8,6 @@ export default function ManageCompanyPage() {
   const [filtered, setFiltered] = useState([]);
   const [showUpdateModal, setShowUpdateModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
-
   const [editData, setEditData] = useState({
     id: "",
     companyName: "",
