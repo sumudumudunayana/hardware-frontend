9e55e9f7f9000ce2ae48857f7c65a3337ad9f54c
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-07 22:18:19 +0530

    cleaned the code

--- src/pages/ManageDistributorPage.jsx
+++ src/pages/ManageDistributorPage.jsx
@@ -166,7 +166,6 @@ export default function ManageDistributorPage() {
                       >
                         Update
                       </button>
-
                       <button
                         className="delete-btn"
                         onClick={() => openDeleteModal(d.id)}
