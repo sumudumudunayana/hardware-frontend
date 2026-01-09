0d1df92a696155af4c2cbdbe2a6360adab5a9191
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-09 22:00:54 +0530

    cleaned the code

--- src/pages/OptionPage.jsx
+++ src/pages/OptionPage.jsx
@@ -24,7 +24,6 @@ export default function OptionPage() {
             <h2>Sales Management</h2>
             <p>Handle sales, billing, and transaction records.</p>
           </div>
-
           <div
             className="option-box"
             onClick={() => navigate("/SupplierOptionPage")}
