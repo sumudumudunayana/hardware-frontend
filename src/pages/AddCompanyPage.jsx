638c862a70856ae8e4d91ebcb880ec97c5902e32
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-18 01:39:04 +0530

    cleaned the code

--- src/pages/AddCompanyPage.jsx
+++ src/pages/AddCompanyPage.jsx
@@ -47,7 +47,6 @@ export default function AddCompanyPage() {
             onChange={handleChange}
             required
           />
-
           <textarea
             name="companyDescription"
             placeholder="Company Description"
