2bf0351a67eef8c4854a011228ec35d410327d31
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-19 13:36:47 +0530

    cleaned the code

--- src/pages/AddDistributorPage.jsx
+++ src/pages/AddDistributorPage.jsx
@@ -64,7 +64,6 @@ export default function AddDistributorPage() {
             onChange={handleChange}
             required
           />
-
           <input
             type="email"
             name="distributorEmail"
