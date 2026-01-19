290b2b7bd2321727672e34e32ab241bf5ff707ff
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-01-19 13:38:15 +0530

    cleaned the code

--- src/pages/AddItemPage.jsx
+++ src/pages/AddItemPage.jsx
@@ -117,7 +117,6 @@ export default function AddItemPage() {
               required
             />
           </div>
-
           <input
             type="number"
             name="itemLabeledPrice"
