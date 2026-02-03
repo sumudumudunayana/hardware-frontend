ea1723aaa21a676d4a84365f334b68c30c53cc14
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-03 23:00:09 +0530

    code updated

--- src/pages/ManageCustomerPage.jsx
+++ src/pages/ManageCustomerPage.jsx
@@ -67,7 +67,6 @@ export default function ManageCustomerPage() {
       );
       setCustomers(updated);
       setFiltered(updated);
-
       setAlert({
         show: true,
         type: "success",
