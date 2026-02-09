34a3a79064f2e0f7ed9dd909e90694e7b3fdd4a3
sumudumudunayana <mudunayanamihigurathna@gmail.com>
2026-02-09 22:36:38 +0530

    code updated

--- src/pages/DistributorOptionPage.jsx
+++ src/pages/DistributorOptionPage.jsx
@@ -4,7 +4,7 @@ import "../css/DistributorOptionPageStyles.css";
 
 export default function DistributorOptionPage() {
   const navigate = useNavigate();
-  
+
   return (
     <div className="distributor-container">
       <div className="distributor-overlay"></div>
@@ -13,13 +13,15 @@ export default function DistributorOptionPage() {
         <div className="distributor-grid">
           <div
             className="distributor-box"
-            onClick={() => navigate("/AddDistributorPage")}>
+            onClick={() => navigate("/AddDistributorPage")}
+          >
             <h2>Add Distributor</h2>
             <p>Add and register new distributor details.</p>
           </div>
           <div
             className="distributor-box"
-            onClick={() => navigate("/ManageDistributorPage")}>
+            onClick={() => navigate("/ManageDistributorPage")}
+          >
             <h2>Manage Distributors</h2>
             <p>View, update, and delete distributor records.</p>
           </div>
