9e08d257496f843041c019426922e9039f9027de
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-07 00:48:27 +0530

    update item page updated

--- src/pages/item/UpdateItemPage.jsx
+++ src/pages/item/UpdateItemPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../css/UpdateItemPageStyles.css";
+import "../../css/item/UpdateItemPageStyles.css";
 
 export default function UpdateItemPage() {
   const [items, setItems] = useState([]);
