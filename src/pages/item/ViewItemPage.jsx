36cf7969faddbdc581e966f4b9862fdbe12f7917
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-07 00:49:14 +0530

    view item page updated

--- src/pages/item/ViewItemPage.jsx
+++ src/pages/item/ViewItemPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../css/ViewItemPageStyles.css";
+import "../../css/item/ViewItemPageStyles.css";
 
 export default function ViewItemPage() {
   const [items, setItems] = useState([]);
