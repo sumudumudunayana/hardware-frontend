541cf2dc5c3c25cc1a73068ac141b53e5ccf890c
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-06 14:08:27 +0530

    landing page updated

--- src/pages/common/LandingPage.jsx
+++ src/pages/common/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/LandingPageStyles.css";
+import "../../css/common/LandingPageStyles.css";
 
 export default function LandingPage() {
   const navigate = useNavigate();
