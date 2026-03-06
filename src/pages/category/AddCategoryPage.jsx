eda05fdc73ff00b66c935f6304f857849f5688ae
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-06 14:07:51 +0530

    add category page updated

--- src/pages/category/AddCategoryPage.jsx
+++ src/pages/category/AddCategoryPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "../css/AddCategoryPageStyles.css";
+import "../../css/category/AddCategoryPageStyles.css";
 
 export default function AddCategoryPage() {
   const [formData, setFormData] = useState({
