6abd98e9079d01e6e21df8c1671a88fb599fe3cf
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-06 14:10:52 +0530

    distributor option page updated

--- src/pages/distributor/DistributorOptionPage.jsx
+++ src/pages/distributor/DistributorOptionPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/DistributorOptionPageStyles.css";
+import "../../css/distributor/DistributorOptionPageStyles.css";
 
 export default function DistributorOptionPage() {
   const navigate = useNavigate();
@@ -9,21 +9,21 @@ export default function DistributorOptionPage() {
     <div className="distributor-container">
       <div className="distributor-overlay"></div>
       <div className="distributor-card">
-        <h1 className="distributor-title">Distributor Management</h1>
+        <h1 className="distributor-title">Supplier Management</h1>
         <div className="distributor-grid">
           <div
             className="distributor-box"
             onClick={() => navigate("/AddDistributorPage")}
           >
-            <h2>Add Distributor</h2>
-            <p>Add and register new distributor details.</p>
+            <h2>Add Supplier</h2>
+            <p>Add and register new supplier details.</p>
           </div>
           <div
             className="distributor-box"
             onClick={() => navigate("/ManageDistributorPage")}
           >
-            <h2>Manage Distributors</h2>
-            <p>View, update, and delete distributor records.</p>
+            <h2>Manage suppliers</h2>
+            <p>View, update, and delete supplier records.</p>
           </div>
         </div>
       </div>
