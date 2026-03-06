5fa35f7c3df7c442c962469a6f46461b8c76d426
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-07 00:45:33 +0530

    add customer page updated

--- src/pages/customer/AddCustomerPage.jsx
+++ src/pages/customer/AddCustomerPage.jsx
@@ -16,6 +16,18 @@ export default function AddCustomerPage() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
+     // PHONE VALIDATION (10 digits)
+    if (!/^\d{10}$/.test(formData.customerContactNumber)) {
+      alert("Contact number must be exactly 10 digits.");
+      return;
+    }
+
+    // EMAIL VALIDATION
+    if (!/^\S+@\S+\.\S+$/.test(formData.customerEmail)) {
+      alert("Please enter a valid email address.");
+      return;
+    }
+
     try {
       await axios.post("http://localhost:5500/api/customers", formData);
 
