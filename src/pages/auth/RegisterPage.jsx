3fc892d8d00523085c9b1e8791036eaa0680b4ad
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-06 14:06:36 +0530

    code updated

---
+++ src/pages/auth/RegisterPage.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import axios from "axios";
+import "../../css/auth/RegisterPageStyles.css";
+import { useNavigate } from "react-router-dom";
+
+export default function RegisterPage() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post("http://localhost:5500/api/auth/register", formData);
+
+      alert("Registration successful");
+
+      navigate("/");
+    } catch (error) {
+      alert(error.response?.data?.message || "Registration failed");
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <div className="auth-card">
+        <h1>Create Account</h1>
+
+        <form onSubmit={handleRegister}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            onChange={handleChange}
+            required
+          />
+
+          <input
+            type="email"
+            name="email"
+            placeholder="Email"
+            onChange={handleChange}
+            required
+          />
+
+          <input
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={handleChange}
+            required
+          />
+
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    </div>
+  );
+}
