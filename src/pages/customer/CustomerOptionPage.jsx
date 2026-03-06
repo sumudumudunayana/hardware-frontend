a4e5bec28049d63c5359fd3bccfbeb3c2db53c2d
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-06 14:10:07 +0530

    customer option page updated

--- src/pages/customer/CustomerOptionPage.jsx
+++ src/pages/customer/CustomerOptionPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../css/CustomerOptionPageStyles.css";
+import "../../css/customer/CustomerOptionPageStyles.css";
 
 export default function CustomerOptionPage() {
   const navigate = useNavigate();
