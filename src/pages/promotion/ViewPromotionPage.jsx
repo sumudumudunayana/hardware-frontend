b1620dea1e972de8b9b44c4c90cc49dd7ac95f1a
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-09 23:04:09 +0530

    view promotion page updated

--- src/pages/promotion/ViewPromotionPage.jsx
+++ src/pages/promotion/ViewPromotionPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../css/ViewPromotionPageStyles.css";
+import "../../css/promotion/ViewPromotionPageStyles.css";
 
 export default function ViewPromotionPage() {
   const [promotions, setPromotions] = useState([]);
