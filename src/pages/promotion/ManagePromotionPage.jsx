99b4bab35564096a8471e432677b1b581f69a5ef
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-09 23:03:40 +0530

    manage promotion page updated

--- src/pages/promotion/ManagePromotionPage.jsx
+++ src/pages/promotion/ManagePromotionPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import "../css/ManagePromotionPageStyles.css";
+import "../../css/promotion/ManagePromotionPageStyles.css";
 
 export default function ManagePromotionPage() {
   const [promotions, setPromotions] = useState([]);
