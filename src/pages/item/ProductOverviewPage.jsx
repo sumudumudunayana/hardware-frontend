40ad8caedec71fcce3db8acefe43b5cdf8d5366b
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-18 14:27:06 +0530

    product overview page added

---
+++ src/pages/item/ProductOverviewPage.jsx
@@ -0,0 +1,8 @@
+export default function ProductOverviewPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Product Dashboard</h1>
+      <p>Select an action from the sidebar.</p>
+    </div>
+  );
+}
