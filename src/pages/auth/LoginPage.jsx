379b2122a352addb61089edae35e7fae1b9411d2
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-23 20:44:43 +0530

    login page updated

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -55,6 +55,7 @@ export default function LoginPage() {
               required
             />
           </div>
+
           <div className="input-box">
             <input
               type="password"
@@ -64,6 +65,7 @@ export default function LoginPage() {
               required
             />
           </div>
+          
           <div className="login-options">
             <label>
               <input type="checkbox" /> Remember me
