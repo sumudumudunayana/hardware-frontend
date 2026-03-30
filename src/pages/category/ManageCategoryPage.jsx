828d7948aa80d23de69cb73039c4271751779178
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-03-30 22:35:47 +0530

    manage category page updated

--- src/pages/category/ManageCategoryPage.jsx
+++ src/pages/category/ManageCategoryPage.jsx
@@ -79,7 +79,7 @@ export default function ManageCategoryPage() {
     } catch (err) {}
   };
 
-  // DELETE WITH SONNER
+  //DELETE WITH SONNER
   const confirmDelete = async () => {
     try {
       await toast.promise(
