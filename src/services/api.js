52c4848c7b7719c7be04deb6ae2dc7c9dfb21db7
Sumudu Mudunayana <mudunayanamihigurathna@gmail.com>
2026-07-03 00:47:19 +0530

    api.js updated

--- src/services/api.js
+++ src/services/api.js
@@ -11,11 +11,9 @@ const api = axios.create({
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("token");
-
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-
     return config;
   },
   (error) => Promise.reject(error),
